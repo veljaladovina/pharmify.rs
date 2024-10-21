af09a12b5320198b5eeb7b3278fbab15e7056c4e
Veljko Djordjevic <ladovinavelja@gmail.com>
2024-10-21 16:50:32 +0200

    Uklonjen API ključ iz .env fajla

---
+++ models/User.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+// Definišemo šemu korisnika
+const UserSchema = new mongoose.Schema({
+  username: { 
+    type: String, 
+    required: true, 
+    unique: true 
+  },
+  email: { 
+    type: String, 
+    required: true, 
+    unique: true 
+  },
+  password: { 
+    type: String, 
+    required: true 
+  },
+  date: { 
+    type: Date, 
+    default: Date.now 
+  }
+});
+
+// Pre nego što sačuvamo korisnika, šifrujemo lozinku
+UserSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+// Metoda za proveru lozinke
+UserSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
